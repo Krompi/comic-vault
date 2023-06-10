7b0aa13dcba09718131f85f99898509e67dfba3c
Krompi <krompi@gmail.com>
2023-06-10 21:21:30 +0200

    Squashed commit of the following:
    commit 5533c139cf323269713c7c1bb0b69279dfd67876
    Author: Krompi <krompi@gmail.com>
    Date:   Tue May 30 23:18:33 2023 +0200
    
        Layout angepaßt

---
+++ resources/js/Components/Breadcrumb.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="w-full rounded-md">
+        <ol class="list-reset flex">
+            <li v-if="links || active">
+                <Link :href="route('home')"
+                    class="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600">
+                    Home
+                </Link>
+            </li>
+            <li v-else>
+                <span class="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600">
+                    Home
+                </span>
+            </li>
+            <li v-if="links" v-for="link in links">
+                <span class="mx-2 text-neutral-500 dark:text-neutral-400">/</span>
+                <Link :href="link['url']"
+                    class="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600">
+                    {{ link['name'] }}
+                </Link>
+            </li>
+            <li class="text-neutral-500 dark:text-neutral-400" v-if="active">
+                <span class="mx-2 text-neutral-500 dark:text-neutral-400">/</span>
+                {{ active }}
+            </li>
+        </ol>
+    </nav>
+</template>
+
+<script setup>
+defineProps({
+    links: {
+        type: Array,
+        default: null
+    },
+    active: {
+        type: String,
+        default: null
+    }
+})
+</script>
---
+++ resources/js/Layouts/BaseContent.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="bg-white shadow" v-if="$slots.header">
+        <div class="max-w-5xl mx-auto py-6">
+            <h1 class="font-semibold text-2xl text-gray-800 leading-tight">
+                <slot name="header">{{ $page.props.app.name }}</slot>
+            </h1>
+        </div>
+    </header>
+    <section class="max-w-5xl mx-auto py-6 text-sm" v-if="$slots.breadcrumb">
+        <slot name="breadcrumb" />
+    </section>
+
+    <section class="py-8">
+        <div class="max-w-5xl mx-auto">
+            <slot />
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+</script>
